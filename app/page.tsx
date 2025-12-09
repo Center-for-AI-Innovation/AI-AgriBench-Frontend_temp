8936c1edd03539575d380591209c8a72c3cf96da
Sol Robinson <srobins6@gmail.com>
2025-12-09 01:51:56 -0800

    this should fix it maybe?

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { createClient } from '@/lib/supabase/server'
 
 export default async function LeaderboardPage() {
 	const supabase = await createClient()
-	const leaderboard = process.env.LEADERBBOARD || 'main'
+	const leaderboard = process.env.LEADERBOARD!
 	const evaluations = (await supabase.from('evaluations').select().eq('leaderboard', leaderboard))
 		.data
 	const evaluationIds = evaluations!.map((evaluation) => evaluation.id)
@@ -22,7 +22,7 @@ export default async function LeaderboardPage() {
 
 	for (let i = 0; i < totalRows; i += maxRows) {
 		const startIndex = i
-		const stopIndex = Math.min(i + maxRows - 1, totalRows)
+		const stopIndex = Math.min(i + maxRows - 1, totalRows) 
 		const pagedScores = (
 			await supabase
 				.from('scores')
